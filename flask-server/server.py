d5dc00f11eb067ce0accdb559aaf4dd0046d6d3b
Sebastian Garcia <sebastian.garcia@wearemo.com>
2022-11-22 00:46:18 -0500

    Adds UI and server changes to display prediction

--- flask-server/server.py
+++ flask-server/server.py
@@ -1,25 +1,71 @@
 from flask import Flask, request
-import os
+import cv2
+import numpy as np
+import json
+import typing as ty
+from flask_cors import CORS
+import base64
+
+from marsemseg.preprocessors import Pipeline, PipelineFactory
+from marsemseg.inference import predict_image
+from marsemseg.segmentors import ISegmentation, SegmentorFactory
 
 app = Flask(__name__)
+CORS(app)
 
 app.config["UPLOAD_FOLDER"] = "static/files"
+
+SHAPE = (384, 512)
+ckpt_file = "torch_script_best_mIoU_iter_4950.pth"
+config_file = "pspnet_r50-d8_512x1024_40k_cityscapes.py"
+
+with open("img_norm_cfg.json", "r") as f:
+    img_norm_dict = json.load(f)
+
+pipeline = PipelineFactory(
+    backend="torchscript",
+    mean=img_norm_dict["mean"],
+    std=img_norm_dict["std"],
+    shape=SHAPE,
+    convert_to_rgb=True,
+).create()
+
+segmentor: ISegmentation = SegmentorFactory(
+    backend="torchscript",
+    checkpoint_path=ckpt_file,
+    config=config_file,
+    shape=SHAPE,
+    device="cuda",
+    half=True,
+).create()
 
 # Test API route
 @app.route("/test")
 def test():
     return {"test": "It works!"}
 
+
 @app.route("/file-upload", methods=["POST"])
 def file_upload():
     if request.files:
+        print()
 
-        file = request.files["myFile"]
-        file.save(os.path.join(os.path.abspath(os.path.dirname(__file__)),app.config["UPLOAD_FOLDER"], file.filename))
+        file_obj = request.files["myFile"]
 
-        print("File saved :)")
+        file_stream = file_obj.stream
+        file_stream.seek(0)
 
-        return {"success": True}
+        img_array = np.array(bytearray(file_stream.read()), dtype=np.uint8)
+
+        img = cv2.imdecode(img_array, cv2.IMREAD_COLOR)
+        predicted = predict_image(img, segmentor, pipeline, SHAPE, show=False)
+        _, result_arr = cv2.imencode('.png', predicted)
+        result_bytes = result_arr.tobytes()
+        b64prediction = base64.b64encode(result_bytes).decode()
+        cv2.imwrite("test.png", predicted)
+
+        return {"success": True, "mask": b64prediction}
+
 
 if __name__ == "__main__":
     app.run(debug=True)
---
+++ frontend/src/views/MainPage.js
@@ -0,0 +1,131 @@
+// import { Component } from 'react';
+
+// function MainPage () {
+
+//     const changeHandler = (event) => {
+// 		// setSelectedFile(event.target.files[0]);
+// 		// setIsSelected(true);
+// 	};
+
+// 	const handleSubmission = () => {
+// 	};
+
+//     return (
+//             <div className="d-flex w-100">
+//                 <h1> MarSemSeg </h1>
+//                 <input type="file" name="file" onChange={changeHandler} />
+//                 <button className=""> Upload file </button>
+//                 <h3>Prediction:</h3>
+//                 <img src="https://compote.slate.com/images/926e5009-c10a-48fe-b90e-fa0760f82fcd.png?width=1200&rect=680x453&offset=0x30" alt="stonks" />
+//             </div>
+//         );
+// }
+
+// export default MainPage;
+
+import axios from 'axios';
+
+import React,{Component} from 'react';
+import CurvedText from '../components/CurvedText';
+
+class MainPage extends Component {
+
+	state = {
+	    selectedFile: null,
+        prediction: {
+            success: false,
+            mask: null
+        } 
+	};
+	
+	// File select
+	onFileChange = event => {
+	    this.setState({ selectedFile: event.target.files[0] });
+	};
+	
+	// File upload
+	onFileUpload = () => {
+        const formData = new FormData();
+        
+        formData.append(
+            "myFile",
+            this.state.selectedFile,
+            this.state.selectedFile.name
+        );
+
+        console.log(this.state.selectedFile);
+        
+        // Send file to backend
+        axios.post("http://localhost:5000/file-upload", formData).then(response => {
+            console.log(response.data);
+                this.setState({prediction: response.data});
+          })
+          .catch(function (error) {
+            console.log(error);
+          });
+	};
+
+    showPrediction = () => {
+        console.log('showing prediction');
+        if (this.state.prediction.success) {
+            return (
+                <img src={"data:image/png;base64," + this.state.prediction.mask} alt="prediction" />
+            );
+        }
+    }
+	
+	// Uploaded file info
+	fileData = () => {
+        if (this.state.selectedFile) {
+            return (
+                <div>
+                    <br />
+                    <h2>File Details:</h2>
+                    
+                    <p>File Name: {this.state.selectedFile.name}</p>
+                    
+                    <p>File Type: {this.state.selectedFile.type}</p>
+                </div>
+            );
+        } else {
+            return (
+                <div>
+                    <br />
+                    <h4 style={{textShadow: '2px 2px 10px #FFFFFF'}}>Choose a file to which you want to apply Maritime Semantic Segmentation</h4>
+                </div>
+            );
+        }
+	};
+	
+	render() {
+        return (
+            <div>
+                {/* <h1 className="font-face-bt" ><CurvedText style={{fontSize: '50px'}} text="MarSemSeg" /></h1> */}
+                {/* <div className="border"> */}
+                    <h1 className="font-face-bt" style={{fontSize: '200px', display: 'inline'}}> MarSemSeg</h1>
+                    <img style={{marginLeft: '0.8rem', paddingBottom: '6rem'}}
+                        src="https://cdn-icons-png.flaticon.com/512/4516/4516008.png"
+                        alt="ancla"
+                        width="130"
+                        height="210"
+                    />
+                {/* </div> */}
+                
+                <div>
+                    <label className="btn btn-info text-white shadow-lg" style={{textShadow: '2px 2px 8px #000000'}}>
+                        <input type="file" name="file" accept="image/*, video/*" onChange={this.onFileChange} />
+                        Select file
+                    </label>
+                </div>
+                {this.fileData()}
+                {this.state.selectedFile && <div>
+                    <button className="btn btn-primary my-3" onClick={this.onFileUpload}> Upload </button>
+                    {this.state.prediction && <h3> Prediction: </h3>}
+                </div>}
+                {this.showPrediction()} 
+            </div>
+        );
+	}
+}
+
+export default MainPage;
